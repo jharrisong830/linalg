ad951b3614b40f5ad3fcf592e86a53e3b4eb66ff
jharrisong830 <110198683+jharrisong830@users.noreply.github.com>
2022-12-09 15:24:53 -0500

    operator overrides for + and *, plus copying

--- matrix.py
+++ matrix.py
@@ -1,6 +1,6 @@
 '''
 AUTHOR:         @jharrisong830
-VERSION:        3.0
+VERSION:        3.1
 DATE:           12/09/22
 DESCRIPTION:    Matrix creator and matrix operations calculator
 '''
@@ -56,7 +56,7 @@ class Matrix:
         max_width=0
         for i in range(self.rows):
             for j in range(self.columns):
-                width=len(str(self.get(i+1, j+1)))
+                width=len(str(round(self.get(i+1, j+1), 3)))
                 if width>max_width:
                     max_width=width
         result=""
@@ -68,9 +68,9 @@ class Matrix:
             else:
                 result+="|"
             for j in range(self.columns):
-                w=" "*(max_width-len(str(self.get(i+1, j+1))))
+                w=" "*(max_width-len(str(round(self.get(i+1, j+1), 3))))
                 result+=w
-                result+=str(self.get(i+1, j+1))
+                result+=str(round(self.get(i+1, j+1), 3))
                 if not j==self.columns-1:
                     result+=" "
             if i==0:
@@ -81,6 +81,37 @@ class Matrix:
                 result+="|\n"
         return result
     
+    def __add__(self, B):
+        '''Returns a new matrix, which is the result of adding the original matrix with B'''
+        if self.rows!=B.rows and self.columns!=B.columns:
+            raise DimensionException
+        C=Matrix(self.rows, self.columns, operation=True)
+        for i in range(self.rows):
+            for j in range(self.columns):
+                C.set(i, j, self.get(i, j)+B.get(i, j))
+        return C
+    
+    def __mul__(self, B):
+        '''Returns a new matrix, which is the result of multiplying the original matrix with B'''
+        if self.columns!=B.rows:
+            raise DimensionException
+        C=Matrix(self.rows, B.columns, operation=True)
+        for i in range(C.rows):
+            for j in range(C.columns):
+                row_vec=self.__row_vector(i+1)
+                col_vec=B.__column_vector(j+1)
+                prod=row_vec.dot_product(col_vec)
+                C.set(i+1, j+1, prod)
+        return C
+    
+    def copy(self):
+        '''Returns a copy of a matrix object'''
+        copy=Matrix(self.rows, self.columns, operation=True)
+        for i in range(copy.rows):
+            for j in range(copy.columns):
+                copy.set(i+1, j+1, self.get(i+1, j+1))
+        return copy
+
     def get(self, m, n):
         '''Gets the entry of the matrix at (m, n)'''
         return self.__A[m-1][n-1]
@@ -103,29 +134,6 @@ class Matrix:
         for i in range(self.rows):
             result.append(self.get(i+1, n))
         return Vector(result)
-
-    def add(self, B):
-        '''Returns a new matrix, which is the result of adding the original matrix with B'''
-        if self.rows!=B.rows and self.columns!=B.columns:
-            raise DimensionException
-        C=Matrix(self.rows, self.columns, operation=True)
-        for i in range(self.rows):
-            for j in range(self.columns):
-                C.set(i, j, self.get(i, j)+B.get(i, j))
-        return C
-    
-    def multiply(self, B):
-        '''Returns a new matrix, which is the result of multiplying the original matrix with B'''
-        if self.columns!=B.rows:
-            raise DimensionException
-        C=Matrix(self.rows, B.columns, operation=True)
-        for i in range(C.rows):
-            for j in range(C.columns):
-                row_vec=self.__row_vector(i+1)
-                col_vec=B.__column_vector(j+1)
-                prod=row_vec.dot_product(col_vec)
-                C.set(i+1, j+1, prod)
-        return C
     
     def transpose(self):
         '''Transposes a matrix'''
@@ -144,10 +152,7 @@ class Matrix:
         '''Returns whether a matrix is symmetric or not'''
         if self.rows!=self.columns:
             raise DimensionException
-        temp=Matrix(self.rows, self.columns, operation=True)
-        for i in range(temp.rows):
-            for j in range(temp.columns):
-                temp.set(i+1, j+1, self.get(i+1, j+1))
+        temp=self.copy()
         temp.transpose()
         for i in range(temp.rows):
             for j in range(temp.columns):
@@ -176,10 +181,7 @@ class Matrix:
     
     def rank(self):
         '''Returns the rank of a matrix'''
-        temp=Matrix(self.rows, self.columns, operation=True)
-        for i in range(temp.rows):
-            for j in range(temp.columns):
-                temp.set(i+1, j+1, self.get(i+1, j+1))
+        temp=self.copy()
         temp.gauss()
         pivots=0
         for i in range(temp.rows):
@@ -246,7 +248,7 @@ class AugMatrix(Matrix):
         max_width=0
         for i in range(self.rows):
             for j in range(self.columns):
-                width=len(str(self.get(i+1, j+1)))
+                width=len(str(round(self.get(i+1, j+1), 3)))
                 if width>max_width:
                     max_width=width
         result=""
@@ -258,16 +260,16 @@ class AugMatrix(Matrix):
             else:
                 result+="|"
             for j in range(self.reg_columns):
-                w=" "*(max_width-len(str(self.get(i+1, j+1))))
+                w=" "*(max_width-len(str(round(self.get(i+1, j+1), 3))))
                 result+=w
-                result+=str(self.get(i+1, j+1))
+                result+=str(round(self.get(i+1, j+1), 3))
                 if not j==self.reg_columns-1:
                     result+=" "
             result+=" | "
             for k in range(self.reg_columns, self.columns):
-                w=" "*(max_width-len(str(self.get(i+1, k+1))))
+                w=" "*(max_width-len(str(round(self.get(i+1, k+1), 3))))
                 result+=w
-                result+=str(self.get(i+1, k+1))
+                result+=str(round(self.get(i+1, k+1), 3))
                 if not k==self.columns-1:
                     result+=" "
             if i==0:
